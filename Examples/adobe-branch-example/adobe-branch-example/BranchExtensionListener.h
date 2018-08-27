dcccdf9f9b4d9844977285ec245ec0a4d9885970
Aaron Lopez <a.aaron.lopez@gmail.com>
2018-08-26 20:50:09 -0700

    Progress

---
+++ Examples/adobe-branch-example/adobe-branch-example/BranchExtensionListener.h
@@ -0,0 +1,13 @@
+//
+//  BranchExtensionListener.h
+//  adobe-branch-example
+//
+//  Created by Aaron Lopez on 8/26/18.
+//  Copyright © 2018 Aaron Lopez. All rights reserved.
+//
+
+#import "ADBExtensionListener.h"
+
+@interface BranchExtensionListener : ADBExtensionListener
+
+@end
---
+++ Examples/adobe-branch-example/adobe-branch-example/BranchExtensionListener.m
@@ -0,0 +1,13 @@
+//
+//  BranchExtensionListener.m
+//  adobe-branch-example
+//
+//  Created by Aaron Lopez on 8/26/18.
+//  Copyright © 2018 Aaron Lopez. All rights reserved.
+//
+
+#import "BranchExtensionListener.h"
+
+@implementation BranchExtensionListener
+
+@end
