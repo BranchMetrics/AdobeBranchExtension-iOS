c46b16a817932318612a85dea647daf15b9c371e
Aaron Lopez <a.aaron.lopez@gmail.com>
2018-09-25 09:39:44 -0700

    table view

--- Examples/adobe-branch-example/adobe-branch-example/ViewController.h
+++ Examples/adobe-branch-example/adobe-branch-example/ViewController.h
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UINavigationController
-
+@interface ViewController : UIViewController
 
 @end
 
--- Examples/adobe-branch-example/adobe-branch-example/ViewController.m
+++ Examples/adobe-branch-example/adobe-branch-example/ViewController.m
@@ -9,7 +9,15 @@
 #import "ViewController.h"
 #import <ACPCore_iOS/ACPCore_iOS.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
+{
+    NSDictionary *productDictionary;
+    NSArray *nameArray;
+    NSArray *staticArray;
+}
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -20,14 +28,39 @@
     // Do any additional setup after loading the view, typically from a nib.
     //[ADBMobileMarketing analyticsTrackAction:@"HI" data:nil];
     
-    [ACPCore trackAction:@"action name" data:@{@"key":@"value"}];
+    productDictionary = [NSDictionary dictionaryWithContentsOfURL:[NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                                                                          pathForResource:@"Products" ofType:@"plist"]isDirectory:NO]];
+    // TODO'
+    self.searchBar.delegate = self;
+    
+//    staticArray = [productDictionary allKeys];
+//    nameArray = [productDictionary allKeys];
+    staticArray = @[@"Hi", @"hello", @"hola", @"apple"];
+    nameArray = @[@"Hi", @"hello", @"hola", @"apple"];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    //[super viewWillAppear:<#animated#>];
+//    searchBar.text = ""
+//    filterTableView(text: "")
 }
 
+- (void) searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    NSPredicate *bPredicate =
+    [NSPredicate predicateWithFormat:@"SELF BEGINSWITH %@", searchText];
+    
+    nameArray = [staticArray filteredArrayUsingPredicate:bPredicate];
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    static NSString *cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    cell.textLabel.text = staticArray[indexPath.row];
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return nameArray.count;
+}
 
 @end
